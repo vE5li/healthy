8405b0eb044dc2a806136ba7693ad6dda51f5bf2
vE5li <vE5li@tuta.io>
2025-11-16 10:09:25 +0100

    Fix rust package

--- flake.nix
+++ flake.nix
@@ -33,6 +33,14 @@
         version = "0.1.0";
         src = ./.;
         cargoLock.lockFile = ./Cargo.lock;
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+
+        buildInputs = with pkgs; [
+          openssl
+        ];
       };
 
       nixosModules.default = {
